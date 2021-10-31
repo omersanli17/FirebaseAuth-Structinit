85768a685e53d407e148a883642a7c748dd07e86
Ömer Şanlı <drasonli17@gmail.com>
2021-10-31 23:00:01 +0300

    Create app.swift

---
+++ app.swift
@@ -0,0 +1,15 @@
+class FirebaseManager: NSObject {
+
+    let auth: Auth
+
+    static let shared = FirebaseManager()
+
+    override init() {
+        FirebaseApp.configure()
+
+        self.auth = Auth.auth()
+
+        super.init()
+    }
+
+}
